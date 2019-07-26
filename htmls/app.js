7db2132f4cc02c1ce379e9ea3242529db1a1f960
phona <675428202@qq.com>
2019-07-27 00:39:46 +0800

    update

--- htmls/app.js
+++ htmls/app.js
@@ -6,13 +6,12 @@ let url
 if (process.env.NODE_ENV === 'DEV') {
     url = 'http://localhost:9000/'
 } else {
-    url = `file://${process.cwd()}/dist/index.html`
+    url = 'http://47.101.218.207:8080'
 }
 
 app.on('ready', () => {
     let window = new BrowserWindow({ width: 800, height: 600 });
     window.loadURL(url);
-    window.webContents.openDevTools();
     window.on('closed', () => {
         // 取消引用 window 对象，如果你的应用支持多窗口的话，
         // 通常会把多个 window 对象存放在一个数组里面，
